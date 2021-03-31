64738aec26f261f26065efc764b9df6493444181
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2021-03-31 22:56:57 +0200

    Add Steps to screens

--- include/sw_screens.h
+++ include/sw_screens.h
@@ -10,7 +10,7 @@
 #include <display/display.h>
 #include "sw_rtc.h"
 
-void sw_splashscreen(display_t *dsp);
-void sw_watch1_init(display_t *dsp, now_t *now);
+void sw_splashscreen(display_t *dsp, now_t *n);
+void sw_watch1_init(display_t *dsp, now_t *now, uint16_t steps);
 void sw_watch1();
 void sw_watch1_ble_status_cb();
